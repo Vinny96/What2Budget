11a16dae21bc0763a2c2f23dd5ce346748561fbf
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2021-02-22 15:07:10 -0500

    Created date formatting and allowed date to be stored in user defaults and displayed at the appropriate labels

--- What2Budget/Controller/datePickerController.swift
+++ What2Budget/Controller/datePickerController.swift
@@ -6,3 +6,49 @@
 //
 
 import Foundation
+import UIKit
+
+class DatePickerController : UIViewController
+{
+    //variables
+    internal var dateKey : String = String()
+    private let defaults = UserDefaults.standard
+    
+    //IB Outlets
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = dateKey
+    }
+    
+    
+    // MARK: - IB Actions
+    @IBAction func saveDate(_ sender: UIButton)
+    {
+        let date = datePicker.date
+        let dateFormatted = date.returnDate()
+        print(dateFormatted)
+        defaults.setValue(dateFormatted, forKey: dateKey)
+    }
+    
+    
+    
+}
+//MARK: - Date Extension
+extension Formatter
+{
+    static let date = DateFormatter()
+}
+
+extension Date
+{
+    func returnDate(dateStyle : DateFormatter.Style = .short, timeStyle : DateFormatter.Style = .none, dateLocale : Locale = Locale.current) -> String
+    {
+        Formatter.date.locale = dateLocale
+        Formatter.date.dateStyle = dateStyle
+        Formatter.date.timeStyle = timeStyle
+        return Formatter.date.string(from: self)
+    }
+}
