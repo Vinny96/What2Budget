a16fb43a70dfedfc8a86144db5696456fdceb51e
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2021-02-26 14:19:31 -0500

    Implemented manual entry functionality plus create and read functionality

--- What2Budget/Controller/expenseTableViewController.swift
+++ What2Budget/Controller/expenseTableViewController.swift
@@ -14,8 +14,9 @@ class expenseTableViewController : UIViewController
     // variables
     var arrayOfExpenses : [ExpenseModel] = []
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    let startDateAsString : String = String()
-    let endDateAsString : String = String()
+    var startDateAsString : String = String()
+    var endDateAsString : String = String()
+    var typeOfExpense : String = String()
     
     // IB Outlets
     @IBOutlet weak var startDate: UILabel!
@@ -23,18 +24,162 @@ class expenseTableViewController : UIViewController
     @IBOutlet weak var tableView: UITableView!
     
     
-    
-    
-    
-    //MARK: - Functions
-    private func initialize()
-    {
-        tableView.delegate = self
-        tableView.register(UINib(nibName: "expenseCell", bundle: .main), forCellReuseIdentifier: "expenseCellToUse")
-        startDate.text = startDateAsString
-        endDate.text = endDateAsString
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initialize()
     }
     
+    
+    //MARK: - Functions and IB Actions
+    private func initialize()
+    {
+        title = typeOfExpense
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "expenseCell", bundle: .main), forCellReuseIdentifier: "expenseCellToUse")
+        tableView.rowHeight = 94
+        startDate.text = startDateAsString
+        endDate.text = endDateAsString
+        loadContext()
+    }
+    
+    private func createManualEntry()
+    {
+        // remember that we have to update the providerTitle, the amountSpent and the notes. We also have to persist this to coreData.
+        var textFieldOne = UITextField()
+        var textFieldTwo = UITextField()
+        var textFieldThree = UITextField()
+        let expenseObjToAdd = ExpenseModel(context: context)
+        
+        
+        let alertControllerThree = UIAlertController(title: "Please enter any notes regarding the purchase.", message: nil, preferredStyle: .alert)
+        alertControllerThree.addTextField { (thirdTextField) in
+            textFieldThree = thirdTextField
+            textFieldThree.placeholder = "notes"
+        }
+        let alertActionFive = UIAlertAction(title: "Save", style: .default) { (fourthAlertAction) in
+            if(textFieldThree.hasText == true)
+            {
+                expenseObjToAdd.notes = textFieldThree.text!
+                
+                self.arrayOfExpenses.append(expenseObjToAdd)
+                self.saveContext()
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+            else
+            {
+                let internalAlertController = UIAlertController(title: "Invalid entry please start over.", message: "", preferredStyle: .alert)
+                let internalAlertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                internalAlertController.addAction(internalAlertAction)
+            }
+        }
+        let alertActionSix = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertControllerThree.addAction(alertActionFive)
+        alertControllerThree.addAction(alertActionSix)
+        
+        
+        let alertControllerTwo = UIAlertController(title: "Please type how much you spent.", message: "Please exclude the dollar sign", preferredStyle: .alert)
+        alertControllerTwo.addTextField { (secondTextField) in
+            textFieldTwo = secondTextField
+            textFieldTwo.keyboardType = .decimalPad
+            textFieldTwo.placeholder = "150.75"
+        }
+        let alertActionThree = UIAlertAction(title: "Save & Continue", style: .default) { (alertActionThreeHandler) in
+            if(textFieldTwo.hasText == true)
+            {
+                expenseObjToAdd.amountSpent = (textFieldTwo.text! as NSString).floatValue
+                self.present(alertControllerThree, animated: true, completion: nil)
+            }
+            else
+            {
+                let internalAlertController = UIAlertController(title: "Invalid entry please start over.", message: "", preferredStyle: .alert)
+                let internalAlertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                internalAlertController.addAction(internalAlertAction)
+            }
+        }
+        
+        let alertActionFour = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertControllerTwo.addAction(alertActionThree)
+        alertControllerTwo.addAction(alertActionFour)
+        
+        
+        let alertControllerOne = UIAlertController(title: "Please type the name of the company.", message: "For example if you paid $10 to apple write type Apple", preferredStyle: .alert)
+        alertControllerOne.addTextField { (firstTextField) in
+            textFieldOne = firstTextField
+            textFieldOne.keyboardType = .default
+            textFieldOne.placeholder = "Type name of company."
+        }
+        let alertActionOne = UIAlertAction(title: "Save & Continue", style: .default) { (alertActionOne) in
+            if(textFieldOne.hasText == true)
+            {
+                expenseObjToAdd.companyName = textFieldOne.text!
+                expenseObjToAdd.typeOfExpense = self.typeOfExpense
+                //expenseObjToAdd.receipt = nil
+                self.present(alertControllerTwo, animated: true, completion: nil)
+            }
+            else
+            {
+                let internalAlertController = UIAlertController(title: "Invalid entry please start over.", message: "", preferredStyle: .alert)
+                let internalAlertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                internalAlertController.addAction(internalAlertAction)
+            }
+        }
+        let alertActionTwo = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertControllerOne.addAction(alertActionOne)
+        alertControllerOne.addAction(alertActionTwo)
+      
+        present(alertControllerOne, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func addExpense(_ sender: Any) {
+        let alertController = UIAlertController(title: "Add \(typeOfExpense)", message: "Please select the photo option if you want us to auto populate the information for you. Please select the manual entry option if you would like to do this yourself.", preferredStyle: .alert)
+        let alertActionOne = UIAlertAction(title: "Take Photo", style: .default) { (alertActionOne) in
+            print("The take photo option was pressed.")
+        }
+        let alertActionTwo = UIAlertAction(title: "Manual Entry", style: .default) { (alertActionTwo) in
+            print("The manual entry option was pressed.")
+            self.createManualEntry()
+        }
+        
+        let alertActionThree = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(alertActionOne)
+        alertController.addAction(alertActionTwo)
+        alertController.addAction(alertActionThree)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+ // MARK: - CRUD Functionality
+    private func saveContext()
+    {
+        do
+        {
+            try context.save()
+        }
+        catch
+        {
+            print("There was an error in saving to the context.")
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func loadContext(request : NSFetchRequest<ExpenseModel> = ExpenseModel.fetchRequest())
+    {
+        request.predicate = NSPredicate(format: "typeOfExpense MATCHES %@", typeOfExpense)
+        do
+        {
+            try arrayOfExpenses = context.fetch(request)
+        }
+        catch
+        {
+            print("There was an error in loading the items from the context.")
+            print(error.localizedDescription)
+        }
+    }
 }
 //MARK: - TableView Extensions
 extension expenseTableViewController : UITableViewDelegate
